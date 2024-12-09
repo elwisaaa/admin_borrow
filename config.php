31f85b7d021e51c13b260a5abce084cb762a6baf
elwisaaa <166204210+elwisaaa@users.noreply.github.com>
2024-12-09 13:38:35 +0800

    s

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "resource_management";
+$dbname = "borrow_resource";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
